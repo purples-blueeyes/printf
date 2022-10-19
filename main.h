b98d412983d4194fadb3a9074fbd52ad97cee61f
Anthoine <dzimahanthoine@gmail.com>
2022-10-18 18:15:54 -0700

    main.h header file updated

--- main.h
+++ main.h
@@ -19,7 +19,7 @@
 
 /**
  * struct parameters - parameters struct
- * @unsign: flag if unsigned value
+ * @unsign: flag of unsigned value
  *
  *   / * FLAGS*
  * @space_flag: on if hashtag_flag specified
@@ -74,10 +74,10 @@ int print_number_right_shift(char *str, params_t *params);
 int print_number_left_shift(char *str, params_t *params);
 
 /* params.c module */
-void init_params(params_t *params, va_list ap);
+void init_params(params_t *params, va_list list);
 
 /* string_fields.c modoule */
-char *get_precision(char *p, params_t *params, va_list ap);
+char *get_precision(char *k, params_t *params, va_list list);
 
 /* _prinf.c module */
 int _printf(const char *format, ...);
@@ -87,33 +87,33 @@ int _puts(char *str);
 int _putchar(int c);
 
 /* print_functions.c module */
-int print_char(va_list ap, params_t *params);
-int print_int(va_list ap, params_t *params);
-int print_string(va_list ap, params_t *params);
-int print_percent(va_list ap, params_t *params);
-int print_S(va_list ap, params_t *params);
+int print_char(va_list list, params_t *params);
+int print_int(va_list list, params_t *params);
+int print_string(va_list list, params_t *params);
+int print_percent(va_list list, params_t *params);
+int print_S(va_list list, params_t *params);
 
 /* convert_number.c module */
-int print_hex(va_list ap, params_t *params);
-int print_HEX(va_list ap, params_t *params);
-int print_binary(va_list ap, params_t *params);
-int print_octal(va_list ap, params_t *params);
+int print_hex(va_list list, params_t *params);
+int print_HEX(va_list list, params_t *params);
+int print_binary(va_list list, params_t *params);
+int print_octal(va_list list, params_t *params);
 
 /* simple_printers.c module */
 int print_from_to(char *start, char *stop, char *except);
-int print_rev(va_list ap, params_t *params);
-int print_rot13(va_list ap, params_t *params);
+int print_rev(va_list list, params_t *params);
+int print_rot13(va_list list, params_t *params);
 
 /* number.c module */
 char *convert(long int num, int base, int flags, params_t *params);
-int print_unsigned(va_list ap, params_t *params);
-int print_address(va_list ap, params_t *params);
+int print_unsigned(va_list list, params_t *params);
+int print_address(va_list list, params_t *params);
 
 /* specifier.c module */
-int (*get_specifier(char *s))(va_list ap, params_t *params);
-int get_print_func(char *s, va_list ap, params_t *params);
+int (*get_specifier(char *s))(va_list list, params_t *params);
+int get_print_func(char *s, va_list list, params_t *params);
 int get_flag(char *s, params_t *params);
 int get_modifier(char *s, params_t *params);
-char *get_width(char *s, params_t *params, va_list ap);
+char *get_width(char *s, params_t *params, va_list list);
 
 #endif /*_MAIN_H_*/
