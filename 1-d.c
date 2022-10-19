0fb22bf8e8394a79d1d543dad6e5756b1c1ce14f
Anthoine <dzimahanthoine@gmail.com>
2022-10-18 19:06:52 -0700

    printf

---
+++ 1-d.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * init_params - clears struct fields and reset buffer
+ * @params: the parameters struct
+ * @list: the argument pointer
+ *
+ * Return: void.
+ */
+
+void init_params(params_t *params, va_list list)
+{
+	params->unsign = 0;
+
+	params->plus_flag = 0;
+	params->space_flag = 0;
+	params->hashtag_flag = 0;
+	params->zero_flag = 0;
+	params->minus_flag = 0;
+
+	params->width = 0;
+	params->precision = UINT_MAX;
+
+	params->h_modifier = 0;
+	params->l_modifier = 0;
+	(void)list;
+}
